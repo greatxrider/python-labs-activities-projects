b2d807244f973171220f238f20802ee20a6d482d
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-09-27 17:59:38 +0800

    Add files via upload

---
+++ Static Code Analysis/sample2.py
@@ -0,0 +1,9 @@
+"""Function to add two numbers."""
+def add(number1, number2):
+    """Return the sum of two numbers."""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
